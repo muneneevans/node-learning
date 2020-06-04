7a86228499c3ac3ec7202895882a5317f68a2455
Evans <evans@vorane.com>
2020-06-04 13:52:28 +0300

    TCW process terminal argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,42 +2,14 @@ const validator = require("validator")
 const notes = require("./notes.js")
 const chalk = require("chalk")
 
-log = console.log
+// console.log(process.argv)
+console.log(chalk.cyan(process.argv))
 
-// Compose multiple styles using the chainable API
-// log(chalk.blue.bgRed.bold('Hello world!'));
-
-// Pass in multiple arguments
-// log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
-
-// Nest styles
-// log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
-
-// Nest styles of the same type even (color, underline, background)
-// log(chalk.green(
-// 	'I am a green line ' +
-// 	chalk.blue.underline.bold('with a blue substring') +
-// 	' that becomes green again!'
-// ));
-
-// ES2015 template literal
-// log(`
-// CPU: ${chalk.red('90%')}
-// RAM: ${chalk.green('40%')}
-// DISK: ${chalk.yellow('70%')}
-// `);
-
-// ES2015 tagged template literal
-// log(chalk`
-// CPU: {red ${cpu.totalPercent}%}
-// RAM: {green ${ram.used / ram.total * 100}%}
-// DISK: {rgb(255,131,0) ${disk.used / disk.total * 100}%}
-// `);
-
-// Use RGB colors in terminal emulators that support it.
-// log(chalk.keyword('orange')('Yay for orange colored text!'));
-// log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
-// log(chalk.hex('#DEADED').bold('Bold gray!'));
-
-
-log(chalk.red.bgBlack.bold("success"))
+const comman = process.argv[2]
+if (comman === "add") {
+	console.log(chalk.white.bold("Add note"))
+} else if (comman === "remove") {
+	console.log(chalk.white.bold("Remove note"))
+} else {
+	console.log(chalk.cyan(argv))
+}
