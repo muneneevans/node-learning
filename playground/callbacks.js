24167d305c5c6f0e1c3854206aaf0a70afff9e42
Evans <evans@vorane.com>
2020-07-05 01:14:04 +0300

    CAllback challenge

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -1,23 +1,17 @@
-setTimeout(() => {
-  console.log("Two seconds are up");
-}, 2000);
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
 
-const names = ["evans", "munene", "overmars", "Jen", "Jess"];
-
-const shortNames = names.filter((name) => {
-  return name.length <= 4;
-});
-
-const geocode = (address, callback) => {
+const add = (a, b, callback) => {
   setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0,
-    };
-    callback(data);
+    callback(a + b);
   }, 2000);
 };
 
-geocode("Nairobi", (data) => {
-  console.log(data);
+add(1, 4, (sum) => {
+  console.log(sum); // Should print: 5
 });
