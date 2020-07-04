5dd5721ad7f2ff472dcb7d266ae26ee8be0c84d2
Evans <evans@vorane.com>
2020-07-05 00:20:09 +0300

    TCW make http request from openweather

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,15 @@
 const request = require("request");
 
 const url =
-  "http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02";
+  "https://samples.openweathermap.org/data/2.5/forecast/hourly?id=524901&appid=439d4b804bc8187953eb36d2a8c26a02";
 
 request(
   {
     url: url,
+    json: true,
   },
   (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data);
+    console.log("it is currently " + response.body.);
+    console.log(response.body);
   }
 );
